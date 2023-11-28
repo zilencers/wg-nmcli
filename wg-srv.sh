f3af7f1ce5e86eacdd42e8930a32c93d3a640e37
zilencer <zilencer@protonmail.com>
2023-11-28 15:01:06 -0600

    Added client setup logic and options

--- wg-srv.sh
+++ wg-srv.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 ADD_PEER=0
-CONN_NAME="wg0-server"
-TUNNEL_IP="10.8.0.1/24"
-VIRT_IFNAME="wg0"
-PORT=51820
+AUTOCONNECT="yes"
 
 usage() {
    echo "wg-srv.sh [OPTION] [VALUE]"
    echo " -a|--add-peer       add a peer to the server; Required: -c and -pk" 
-   echo " -c|--conn-name      network manager connection name; Default: wg0-server"
-   echo "-ip|--tunnel-ip      server tunnel ip in CIDR notation; Default: 10.8.0.1/24"
+   echo "-ac|--autoconnect    automatically start the connection at boot [yes|no]; Default: yes"
+   echo "    --allowed-ip     sets tunnel IP addresses of the clients allowed to send data to the server"
+   echo "                     or tunnel IP addresses of the servers to allowed to communicate with client"
+   echo "                     use -r|--route_all to route all traffic through the tunnel "
+   echo " -c|--conn-name      network manager connection name; Default: wg0-[client|server]"
+   echo " -e|--endpoint       sets the hostname or IP address of the server"
+   echo " -g|--gateway        gateway IP; use server tunnel ip as gateway"
+   echo "-ip|--tunnel-ip      server tunnel ip in CIDR notation"
    echo " -p|--port           port the server should listen on; Default: 51820"
    echo "-pk|--peer-pubkey    peer public key"
+   echo " -r|--route-all      route all traffic through tunnel; Only valid for client"
+   echo " -t|--type           type of install; Options: [client|server]"
    echo " -v|--virt-ifname    virtual interface name; Default: wg0"
    echo " -h|--help           display help information"
 
@@ -33,8 +38,24 @@ parse_args() {
 	     ADD_PEER=1
 	     shift 1
 	     ;;
+	  -ac|--autoconnect)
+	     AUTOCONNECT=$2
+	     shift 2
+	     ;;
+	  --allowed-ip)
+	     ALLOWED_IP=$2
+	     shift 2
+	     ;;
 	  -c|--conn-name)
 	     CONN_NAME=$2
+	     shift 2
+	     ;;
+	  -e|--endpoint)
+	     ENDPOINT=$2
+	     shift 2
+	     ;;
+	  -g|--gateway)
+	     GATEWAY=$2
 	     shift 2
 	     ;;
          -ip|--tunnel-ip)
@@ -45,12 +66,16 @@ parse_args() {
 	    PORT=$2
 	    shift 2
 	    ;;
-	 --peer-ip)
-	    PEER_IP=$2
-	    shift 2
-	    ;;
 	 -pk|--peer-pubkey)
 	    PEER_PUBKEY=$2
+	    shift 2
+	    ;;
+	  -r|--route-all)
+	    ROUTE_ALL="0.0.0.0/0;"
+	    shift 1
+	    ;;
+	  -t|--type)
+	    TYPE=$2
 	    shift 2
 	    ;;
 	  -v|--virt-ifname)
@@ -65,9 +90,27 @@ parse_args() {
 }
 
 validate_args() {
+   [ -n $TUNNEL_IP ] && abnormal_exit "missing [-ip|--tunnel-ip] argument" usage
+   [ -n $TYPE ] && abnormal_exit "missing [-t|--type] argument" usage
+   [ -n $VIRT_IFNAME ] && VIRT_IFNAME="wg0"
+   [ -n $PORT ] && PORT=51820
+   [ -n $CONN_NAME ] && CONN_NAME=$VIRT_IFNAME"-"$TYPE
+
+   TYPE=$(echo $TYPE | tr '[:upper:]' '[:lower:]')
+   VIRT_IFNAME=$(echo $VIRT_IFNAME | tr '[:upper:]' '[:lower:]')
+
+   if [[ $ROUTE_ALL ]] && [[ $ALLOWED_IP ]]; then
+      abnormal_exit "--route-all and --allowed-ip cannot be used together" usage
+   fi 
+
+   if [ $TYPE = "client" ]; then
+      [ -n $ENDPOINT ] && abnormal_exit "missing required argument -e|--endpoint" usage
+   fi 
+
    if [ $ADD_PEER -eq 1 ]; then 
       [ ! $PEER_IP ] && abnormal_exit "missing required argument --peer-ip" usage
       [ ! $PEER_PUBKEY ] && abnormal_exit "missing required argument -pk|--peer-pubkey" usage
+      [ ! $TYPE ] && abnormal_exit "missing required argument [ -t|--type]" usage
    fi
 }
 
@@ -95,52 +138,62 @@ genkeys() {
 }
 
 create_connection() {
-    printf "Adding NetworkManager WireGuard connection profile ...\n"
-    nmcli connection add type wireguard con-name $CONN_NAME ifname $VIRT_IFNAME autoconnect no
-    printf "Done\n"
+   printf "Adding NetworkManager WireGuard connection profile ...\n"
+   nmcli connection add type wireguard con-name $CONN_NAME ifname $VIRT_IFNAME autoconnect no
+   printf "Done\n"
 
-    printf "Set the tunnel IPv4 address and subnet mask ..."
-    nmcli connection modify $CONN_NAME ipv4.method manual ipv4.addresses $TUNNEL_IP
-    printf "Done\n"
+   # "Set the tunnel IPv4 address and subnet mask ..."
+   nmcli connection modify $CONN_NAME ipv4.method manual ipv4.addresses $TUNNEL_IP
 
-    printf "Adding private key to the connection profile ..."
-    nmcli connection modify $CONN_NAME wireguard.private-key "$PRIVATE_KEY"
-    printf "Done\n"
+   # If Gateway argument was provided, set the gateway ip
+   [[ $GATEWAY ]] &&  nmcli connection modify $CONN_NAME ipv4.gateway $GATEWAY
 
-    printf "Setting port for incoming WireGuard connections ..."
-    nmcli connection modify $CONN_NAME wireguard.listen-port $PORT
-    printf "Done\n"
+   # "Adding private key to the connection profile ..."
+   nmcli connection modify $CONN_NAME wireguard.private-key "$PRIVATE_KEY"
+
+   # "Setting port for incoming WireGuard connections ..."
+   [ $TYPE = "server" ] && nmcli connection modify $CONN_NAME wireguard.listen-port $PORT
 
     printf "Adding peer configuration ..."
     [ $PEER_PUBKEY ] && add_peer
     [ -n $PEER_PUBKEY ] && printf "No peer key Skipping\n"
 
-    printf "Reloading the connection profile ..."
+    # "Reloading the connection profile ..."
     nmcli connection load /etc/NetworkManager/system-connections/$CONN_NAME.nmconnection
-    printf "Done\n"
 
-    printf "Configuring the connection to start automatically ..."
-    nmcli connection modify $CONN_NAME autoconnect yes
+    printf "Configuring connection autoconnect ..."
+    nmcli connection modify $CONN_NAME autoconnect $AUTOCONNECT
     printf "Done\n"
    
     printf "Reactivating the connection ...\n"
     nmcli connection up $CONN_NAME
-    printf "Done\n"
 }
 
 add_peer() {
-cat << EOF >> /etc/NetworkManager/system-connections/$CONN_NAME.nmconnection
-[wireguard-peer.$PEER_PUBKEY]
-allowed-ips=$PEER_IP;
-EOF
+   local config="/etc/NetworkManager/system-connections/$CONN_NAME.nmconnection"
+
+   if [ $TYPE = "client" ]; then
+      echo "[wireguard-peer.$PEER_PUBKEY]" >> $config
+      echo "endpoint=$ENDPOINT:$PORT" >> $config
+      [[  $ROUTE_ALL ]] && echo "allowed-ips=$ROUTE_ALL" >> $config
+      [[ $ALLOWED_IP ]] && echo "allowed-ips=$ALLOWED_IP" >> $config
+      echo "persistent-keepalive=20" >> $config
+   fi
+
+   if [ $TYPE = "server" ]; then
+      echo "[wireguard-peer.$PEER_PUBKEY]" >> $config
+      echo "allowed-ips=$ALLOWED_IP;" >> $config
+   fi
 
    printf "Done\n"
 }
 
 add_firewall_rules() {
-   firewall-cmd --permanent --add-port=51820/udp --zone=public
-   firewall-cmd --permanent --zone=public --add-masquerade
-   firewall-cmd --reload
+   if [ $TYPE = "server" ]; then
+      firewall-cmd --permanent --add-port=51820/udp --zone=public
+      firewall-cmd --permanent --zone=public --add-masquerade
+      firewall-cmd --reload
+   fi
 }
 
 main() {
